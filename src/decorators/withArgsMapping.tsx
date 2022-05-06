a45ea4f7d61d8ee7734dbfaffb420d541cdd19dc
macabeus <bruno.macabeus@gmail.com>
2022-05-06 14:36:22 +0100

    fix(withArgsMapping): avoid crash on docs tab and when updating an arg

--- src/decorators/withArgsMapping.tsx
+++ src/decorators/withArgsMapping.tsx
@@ -4,7 +4,8 @@ export type MappingFunction = (value: any) => React.ReactChild;
 
 const deepApplyMap = (
   object: { [key: string]: any },
-  mapping: { [key: string]: MappingFunction }
+  mapping: { [key: string]: MappingFunction },
+  deep: number
 ): { [key: string]: any } => {
   return Object.fromEntries(
     Object.entries(object).map(([key, value]) => {
@@ -13,12 +14,14 @@ const deepApplyMap = (
         return [key, mappingFunc(value)];
       }
 
-      if (typeof value === "object") {
+      // TODO: `(deep < 1)` is a workaround to avoid the crash in docs view mode or when changing the story state
+      // https://app.asana.com/0/1202114683897430/1202109279133823/f
+      if (typeof value === "object" && deep < 1) {
         if (Array.isArray(value)) {
-          return [key, value.map((item) => deepApplyMap(item, mapping))];
+          return [key, value.map((item) => deepApplyMap(item, mapping, deep + 1))];
         }
 
-        return [key, deepApplyMap(value, mapping)];
+        return [key, deepApplyMap(value, mapping, deep + 1)];
       }
 
       return [key, value];
@@ -32,7 +35,7 @@ export const withArgsMapping = (mapping: {
   return (story, context) => {
     const updatedContext = {
       ...context,
-      args: deepApplyMap(context.args, mapping),
+      args: deepApplyMap(context.args, mapping, 0),
     };
 
     return story(updatedContext);
