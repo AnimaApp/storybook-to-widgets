91b7a4b6d017ff9694f6d7240d99e2109bd24a6f
besartmarku <besart.marku@gmail.com>
2022-05-13 01:20:37 +0200

    feat(material-ui): install material ui packages

---
+++ widget-libraries/material-ui/metadata.js
@@ -0,0 +1,9 @@
+export default {
+  name: "Material UI",
+  description:
+    "MUI offers a comprehensive suite of UI tools to help you ship new features faster. Start with Material UI, our fully-loaded component library, or bring your own design system to our production-ready components",
+  short_description: "Move faster with intuitive React UI tools",
+  thumbnail_url: "https://animaapp.s3.amazonaws.com/widgets/ant-design.svg",
+  empty_thumb_url: "https://animaapp.s3.amazonaws.com/widgets/ant-design.svg",
+  sample_file_figma_url: "",
+};
