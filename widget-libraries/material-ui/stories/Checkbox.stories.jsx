7b7f1a0fb002044d6f1bd8e734debc9f7e347259
besartmarku <besart.marku@gmail.com>
2022-05-13 12:36:15 +0200

    feat(material-ui): add Checkbox story

---
+++ widget-libraries/material-ui/stories/Checkbox.stories.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import * as MuiMaterial from "@mui/material";
+
+export default {
+  title: "Material UI/Checkbox",
+  component: MuiMaterial.Checkbox,
+  argTypes: {
+    color: {
+      description: "Color",
+      type: "options",
+      options: [
+        "inherit",
+        "primary",
+        "secondary",
+        "success",
+        "error",
+        "info",
+        "warning",
+      ],
+    },
+    size: {
+      description: "Size",
+      type: "options",
+      options: ["small", "medium"],
+    },
+    disabled: {
+      description: "Disabled",
+      type: "boolean",
+    },
+  },
+};
+
+const Template = (args) => (
+  <MuiMaterial.Checkbox
+    color={args.color}
+    disabled={args.disabled}
+    checked={args.checked}
+    size={args.size}
+  />
+);
+
+export const SimpleCheckbox = Template.bind({});
+SimpleCheckbox.args = {
+  color: "primary",
+  size: "medium",
+  disabled: false,
+  checked: true,
+};
