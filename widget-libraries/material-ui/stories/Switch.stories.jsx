98a10521d8a317548f4de40328a49b5615e1a0de
besartmarku <besart.marku@gmail.com>
2022-05-16 11:23:59 +0200

    feat(mui): add Switch story

---
+++ widget-libraries/material-ui/stories/Switch.stories.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import * as MuiMaterial from "@mui/material";
+import withIconMapped from "../decorators/withIconMapped";
+import { options, iconTransform } from "../utils";
+
+export default {
+  title: "Material UI/Switch",
+  component: MuiMaterial.Switch,
+  argTypes: {
+    color: {
+      description: "Color",
+      type: "options",
+      options: [
+        "default",
+        "primary",
+        "secondary",
+        "error",
+        "info",
+        "success",
+        "warning",
+      ],
+    },
+    size: {
+      description: "Size",
+      type: "options",
+      options: ["small", "medium"],
+    },
+    disabled: {
+      description: "Disabled",
+      type: "boolean",
+    },
+    checked: {
+      description: "Checked",
+      type: "boolean",
+    },
+    iconName: {
+      type: "options",
+      options: options,
+      description: "Icon",
+      transform: iconTransform,
+    },
+  },
+  decorators: [withIconMapped],
+};
+
+const Template = (args) => {
+  return (
+    <MuiMaterial.Switch
+      color={args.color}
+      disabled={args.disabled}
+      size={args.size}
+      checked={args.checked}
+    />
+  );
+};
+
+const CustomSwitchTemplate = (args) => {
+  const CustomSwitch = MuiMaterial.styled((props) => (
+    <MuiMaterial.Switch disableRipple {...props} />
+  ))(({ theme }) => ({
+    "& .MuiSwitch-switchBase": {
+      "&.Mui-checked": {
+        marginTop: "-2px",
+        marginLeft: "-2px",
+      },
+    },
+  }));
+
+  return (
+    <CustomSwitch
+      color={args.color}
+      disabled={args.disabled}
+      size={args.size}
+      checked={args.checked}
+      checkedIcon={args.iconName}
+    />
+  );
+};
+
+export const SimpleSwitch = Template.bind({});
+SimpleSwitch.args = {
+  color: "primary",
+  size: "medium",
+  disabled: false,
+  checked: true,
+};
+
+export const SwitchWithCustomIcon = CustomSwitchTemplate.bind({});
+SwitchWithCustomIcon.args = {
+  color: "primary",
+  size: "medium",
+  disabled: false,
+  checked: true,
+  iconName: "AddOutlined",
+};
