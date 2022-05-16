ebf2af4f0f11f355be4832396d2bb825bf0539af
Haim Ben-Chimol <>
2022-05-16 20:56:29 +0300

    chore: update metadata urls

--- widget-libraries/material-ui/metadata.js
+++ widget-libraries/material-ui/metadata.js
@@ -3,7 +3,7 @@ export default {
   description:
     "MUI offers a comprehensive suite of UI tools to help you ship new features faster. Start with Material UI, our fully-loaded component library, or bring your own design system to our production-ready components",
   short_description: "Move faster with intuitive React UI tools",
-  thumbnail_url: "https://animaapp.s3.amazonaws.com/widgets/ant-design.svg",
-  empty_thumb_url: "https://animaapp.s3.amazonaws.com/widgets/ant-design.svg",
+  thumbnail_url: "https://animaapp.s3.amazonaws.com/widgets/material-design.svg",
+  empty_thumb_url: "https://animaapp.s3.amazonaws.com/widgets/empty-material-design.svg",
   sample_file_figma_url: "",
 };
