c1cad97727566542c8a397e2a4cc991e2ace98c0
besartmarku <besart.marku@gmail.com>
2022-05-18 15:31:29 +0200

    feat(mui): add Icon story

---
+++ widget-libraries/material-ui/stories/Icon.stories.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import * as MuiMaterial from "@mui/material";
+import { options, iconTransformSnakeCase } from "../utils";
+import withIconMapped from "../decorators/withIconMapped";
+
+export default {
+  title: "Material UI/Icon",
+  component: MuiMaterial.Icon,
+  argTypes: {
+    color: {
+      description: "Color",
+      type: "options",
+      options: [
+        "inherit",
+        "action",
+        "disabled",
+        "primary",
+        "secondary",
+        "error",
+        "info",
+        "success",
+        "warning",
+      ],
+    },
+    fontSize: {
+      description: "Size",
+      type: "options",
+      options: ["inherit", "large", "medium", "small"],
+    },
+
+    iconName: {
+      type: "options",
+      options: options,
+      description: "Icon",
+      transform: iconTransformSnakeCase,
+    },
+  },
+  decorators: [withIconMapped],
+};
+
+const Template = (args) => {
+  return (
+    <MuiMaterial.Icon color={args.color} fontSize={args.fontSize}>
+      {args.iconName}
+    </MuiMaterial.Icon>
+  );
+};
+
+export const SimpleIcon = Template.bind({});
+SimpleIcon.args = {
+  color: "primary",
+  fontSize: "medium",
+  iconName: "ArrowUp",
+};
--- widget-libraries/material-ui/utils/index.tsx
+++ widget-libraries/material-ui/utils/index.tsx
@@ -3,3 +3,6 @@ import * as MuiIconsMaterial from "@mui/icons-material";
 export const options = ["None", ...Object.keys(MuiIconsMaterial)];
 export const iconTransform =
   "<% if (param !== 'None') { %><MuiIconsMaterial.<%= param %> /><% } else { %> '' <% } %>";
+
+export const iconTransformSnakeCase =
+  "<% if (param !== 'None') { %>'<%= param.split(/(?=[A-Z])/).join('_').toLowerCase()%>'<% } else { %> '' <% } %>";
