7a634c6542da21731ae32caf81a2b9c018b67c2a
Haim Ben-Chimol <>
2022-06-01 12:17:46 +0300

    fix: simplify continues slider

--- widget-libraries/material-ui/stories/ContinousSlider.stories.jsx
+++ widget-libraries/material-ui/stories/ContinousSlider.stories.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import * as MuiMaterial from "@mui/material";
 
 export default {
-  title: "Material UI/Continous Slider",
+  title: "Material UI/Continous slider",
   component: MuiMaterial.Slider,
   argTypes: {
+    width: {
+      description: "Width",
+    },
     color: {
       description: "Color",
       type: "options",
@@ -19,41 +22,47 @@ export default {
       description: "Disabled",
       type: "boolean",
     },
-    orientation: {
-      description: "Orientation",
-      type: "options",
-      options: ["vertical", "horizontal"],
-      defaultValue: "horizontal",
+    defaultValue: {
+      description: "Default value"
     },
-  },
-  decorators: [
-    (Story) => (
-      <div style={{ marginTop: "2em", marginLeft: "1em" }}>
-        <Story />
-      </div>
-    ),
-  ],
+    maxValue: {
+      description: "Max value"
+    },
+    minValue: {
+      description: "Min value"
+    },
+    valueLabelDisplay: {
+      description: "Value label display",
+      type: "options",
+      options: ["auto", "on", "off"],
+    }
+  }
 };
 
 const ContinousSliderTemplate = (args) => {
   return (
-    <MuiMaterial.Slider
-      sx={{
-        '& input[type="range"]': {
-          WebkitAppearance: "slider-vertical",
-        },
-      }}
-      size={args.size}
-      color={args.color}
-      disabled={args.disabled}
-      orientation={args.orientation}
-    />
+    <MuiMaterial.Box width={args.width}>
+      <MuiMaterial.Slider
+        size={args.size}
+        defaultValue={args.defaultValue}
+        color={args.color}
+        disabled={args.disabled}
+        valueLabelDisplay={args.valueLabelDisplay}
+        max={args.maxValue}
+        min={args.minValue}
+      />
+    </MuiMaterial.Box>
   );
 };
 
 export const ContinuousSlider = ContinousSliderTemplate.bind({});
 ContinuousSlider.args = {
+  width: 200,
   color: "primary",
   size: "medium",
+  defaultValue: 70,
+  maxValue: 100,
+  minValue: 0,
+  valueLabelDisplay: "auto",
   disabled: false,
 };
