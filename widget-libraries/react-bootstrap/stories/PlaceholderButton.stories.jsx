0637cf3e9e2574c9ea6c788666351e29789f4b0f
besartmarku <besart.marku@gmail.com>
2022-06-06 15:08:02 +0200

    feat(bootstrap): add placeholder button story

---
+++ widget-libraries/react-bootstrap/stories/PlaceholderButton.stories.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import * as ReactBootstrap from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  title: "React Bootstrap/Placeholder Button ",
+  component: ReactBootstrap.Placeholder.Button,
+  argTypes: {
+    animation: {
+      description: "Animation",
+      type: "options",
+      options: ["glow", "wave"],
+    },
+    variant: {
+      description: "Bg",
+      type: "options",
+      options: [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "dark",
+        "light",
+      ],
+    },
+    size: {
+      description: "Size",
+      type: "options",
+      options: ["xs", "sm", "lg"],
+    },
+    xs: { description: "Columns number", type: "number" },
+  },
+};
+
+const Template = (args) => {
+  return (
+    <ReactBootstrap.Placeholder.Button
+      animation={args.animation}
+      variant={args.variant}
+      size={args.size}
+      xs={args.xs}
+    />
+  );
+};
+
+export const SimplePlaceholderButton = Template.bind({});
+SimplePlaceholderButton.args = {
+  animation: "wave",
+  variant: "primary",
+  size: "lg",
+  xs: 2,
+};
