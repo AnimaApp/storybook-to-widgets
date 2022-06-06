bfd4c7f2909a3b8311fd0487359a246a7641bdec
besartmarku <besart.marku@gmail.com>
2022-06-06 23:46:53 +0200

    feat(mui): add Speedial actions

--- widget-libraries/material-ui/stories/SpeedDial.stories.jsx
+++ widget-libraries/material-ui/stories/SpeedDial.stories.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import * as MuiMaterial from "@mui/material";
+import { SimpleSpeedDialAction } from "./SpeedDialAction.stories";
+import { iconOptions, iconTransform } from "../utils";
+import withIconMapped from "../decorators/withIconMapped";
 
 // SpeedDial
 export default {
@@ -12,37 +15,66 @@ export default {
       options: ["down", "left", "right", "up"],
     },
     hidden: { description: "Hidden", type: "boolean" },
+    speedDial1: {
+      type: "story",
+      description: "menuitem",
+      storyInfo: SimpleSpeedDialAction.storyInfo,
+    },
+    speedDial2: {
+      type: "story",
+      description: "menuitem",
+      storyInfo: SimpleSpeedDialAction.storyInfo,
+    },
+    speedDial3: {
+      type: "story",
+      description: "menuitem",
+      storyInfo: SimpleSpeedDialAction.storyInfo,
+    },
+    iconName: {
+      type: "options",
+      options: iconOptions,
+      description: "Icon",
+      transform: iconTransform,
+      required: false,
+    },
   },
+  decorators: [withIconMapped],
 };
 
 const Template = (args) => {
-  const actions = [
-    { name: "Copy" },
-    { name: "Save" },
-    { name: "Print" },
-    { name: "Share" },
-  ];
-
   return (
     <MuiMaterial.SpeedDial
       ariaLabel="SpeedDial basic example"
       sx={{ position: "absolute", bottom: 16, right: 16 }}
-      //   icon={<SpeedDialIcon />}
-      orientation={args.orientation}
+      icon={<MuiMaterial.SpeedDialIcon />}
+      direction={args.direction}
       hidden={args.hidden}
     >
-      {actions.map((action) => (
-        <MuiMaterial.SpeedDialAction
-          key={action.name}
-          tooltipTitle={action.name}
-        />
-      ))}
+      <SimpleSpeedDialAction {...args.speedDial1} />
+      <SimpleSpeedDialAction {...args.speedDial2} />
+      <SimpleSpeedDialAction {...args.speedDial3} />
     </MuiMaterial.SpeedDial>
   );
 };
 
 export const SimpleSpeedDial = Template.bind({});
 SimpleSpeedDial.args = {
-  orientation: "horizontal",
+  direction: "up",
   hidden: false,
+  iconName: "SpeedDialIcon",
+  speedDial1: {
+    tooltipPlacement: "left",
+    iconName: "Save",
+    tooltipTitle: "Tooltip Title",
+  },
+  speedDial2: {
+    tooltipPlacement: "left",
+    iconName: "Print",
+    tooltipTitle: "Tooltip Title",
+  },
+  speedDial3: {
+    tooltipPlacement: "left",
+    iconName: "Share",
+    tooltipTitle: "Tooltip Title",
+  },
 };
