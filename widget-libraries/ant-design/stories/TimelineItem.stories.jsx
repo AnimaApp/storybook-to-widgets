dfa65030cb9478c6d965bbda40e894f38ecfcc99
Besart Marku <besart.marku@gmail.com>
2022-06-16 12:37:51 +0200

    feat(antd): add Timeline and Timeline Item stories

---
+++ widget-libraries/ant-design/stories/TimelineItem.stories.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "antd/dist/antd.css";
+import * as Antd from "antd";
+
+export default {
+  title: "Ant Design/Timeline Item",
+  component: Antd.Timeline.Item,
+  parameters: {
+    docs: {
+      description: {
+        component: "Item",
+      },
+    },
+    custom_css:
+      '.${nodeClass} { font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";}',
+  },
+  argTypes: {
+    color: {
+      description: "Color",
+      control: {
+        type: "color",
+      },
+    },
+    label: { description: "Label", type: "string" },
+    position: {
+      description: "Position",
+      type: "options",
+      options: ["right", "left"],
+    },
+  },
+};
+
+const Template = (args) => (
+  <Antd.Timeline.Item position={args.position} color={args.color}>
+    {args.label}
+  </Antd.Timeline.Item>
+);
+
+export const SimpleTimelineItem = Template.bind({});
+SimpleTimelineItem.args = {
+  label: "Label",
+  position: "right",
+  color: "blue",
+};
+
+SimpleTimelineItem.storyInfo = {
+  name: "SimpleTimelineItem",
+  kind: "Ant Design/Timeline Item",
+};
