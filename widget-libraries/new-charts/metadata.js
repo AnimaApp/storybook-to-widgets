db7578b552b8466f309531a41e7c3715efe26566
adinu <adisod@gmail.com>
2022-06-20 15:51:50 +0300

    change icons

--- widget-libraries/new-charts/metadata.js
+++ widget-libraries/new-charts/metadata.js
@@ -3,9 +3,9 @@ export default {
   description:
     "Recharts makes it easy to integrate charts into your React application.",
   short_description: "Based on Recharts React library.",
-  thumbnail_url: "https://animaapp.s3.amazonaws.com/widgets/chartsdeprecated.svg",
+  thumbnail_url: "https://animaapp.s3.amazonaws.com/widgets/charts-symbol.svg",
   empty_thumb_url:
-    "https://animaapp.s3.amazonaws.com/widgets/chartsdeprecated.svg",
+    "https://animaapp.s3.amazonaws.com/widgets/charts-symbol.svg",
   sample_file_figma_url:
     "https://www.figma.com/community/file/1029805611065950381",
 };
