9aff994e9e3a4b54305cd813424fbce0c636c24b
Besart Marku <besart.marku@gmail.com>
2022-07-06 09:19:39 +0200

    feat(antd): add List and ListItem stories

---
+++ widget-libraries/ant-design/stories/ListItem.stories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "antd/dist/antd.css";
+import * as Antd from "antd";
+
+export default {
+  title: "Ant Design/List Item",
+  component: Antd.List.Item,
+  parameters: {
+    docs: {
+      description: {
+        component: "Item",
+      },
+    },
+    custom_css:
+      '.${nodeClass} { font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";}',
+  },
+  argTypes: {
+    label: { description: "Label", type: "string" },
+  },
+};
+
+const Template = (args) => <Antd.List.Item>{args.label}</Antd.List.Item>;
+
+export const SimpleListItem = Template.bind({});
+SimpleListItem.args = {
+  label: "Label",
+};
+
+SimpleListItem.storyInfo = {
+  name: "SimpleListItem",
+  kind: "Ant Design/List Item",
+};
