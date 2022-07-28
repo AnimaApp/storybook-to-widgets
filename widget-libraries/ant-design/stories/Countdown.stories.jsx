660507246555e6ca1f946b09e09530d7e55c836c
Besart Marku <besart.marku@gmail.com>
2022-07-28 18:00:30 +0200

    fix(antd): add mapping support for deadline

--- widget-libraries/ant-design/stories/Countdown.stories.jsx
+++ widget-libraries/ant-design/stories/Countdown.stories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "antd/dist/antd.css";
 import * as Antd from "antd";
+import withSecondsMapped from "../decorators/withSecondsMapped";
 
 export default {
   title: "Ant Design/Countdown",
@@ -10,17 +11,21 @@ export default {
       '.${nodeClass} { font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";}',
   },
   argTypes: {
-    deadline: { description: "Deadline", type: "number" },
+    deadline: {
+      description: "Deadline",
+      type: "number",
+    },
     title: { description: "Title", type: "string" },
   },
+  decorators: [withSecondsMapped()],
 };
 
-const Template = (args) => (
-  <Antd.Statistic.Countdown value={args.deadline} title={args.title} />
-);
+const Template = (args) => {
+  return <Antd.Statistic.Countdown value={args.deadline} title={args.title} />;
+};
 
 export const SimpleCountdown = Template.bind({});
 SimpleCountdown.args = {
   title: "Countdown Title",
-  deadline: Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 2,
+  deadline: 60,
 };
