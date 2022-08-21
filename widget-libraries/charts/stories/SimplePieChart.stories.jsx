335ad8aedfc3ae983b152c51b1b642728e6e648f
Besart Marku <besart.marku@gmail.com>
2022-08-21 13:31:30 +0200

    fix(charts): PieChart change defaults

--- widget-libraries/charts/stories/SimplePieChart.stories.jsx
+++ widget-libraries/charts/stories/SimplePieChart.stories.jsx
@@ -12,7 +12,6 @@ export default {
     },
   },
   argTypes: {
-    ShowLegend: { description: "Show Legend" },
     ShowTooltip: { description: "Show Tooltip" },
     dataFillColor: { description: "Fill Color" },
     dataKey: { description: "Data Key" },
@@ -33,9 +32,6 @@ const Template = (args) => (
       label
     />
     {args.ShowTooltip && <Recharts.Tooltip />}
-    {args.ShowLegend && (
-      <Recharts.Legend align={args.align} verticalAlign={args.verticalAlign} />
-    )}
   </Recharts.PieChart>
 );
 
@@ -50,7 +46,7 @@ SinglePie.args = {
     bottom: 5,
   },
   dataFillColor: "#3366FF",
-  dataOuterRadius: 60,
+  dataOuterRadius: 75,
   dataKey: "value",
   data: [
     { name: "Group A", value: 400 },
@@ -63,5 +59,4 @@ SinglePie.args = {
   align: "center",
   verticalAlign: "top",
   ShowTooltip: false,
-  ShowLegend: false,
 };
