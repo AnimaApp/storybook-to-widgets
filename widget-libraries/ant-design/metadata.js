f8c851df2497aed3bfdb323220be60c5adc03754
adinu <adisod@gmail.com>
2022-08-30 11:37:53 +0300

    add theme params, remove not needed userParams

--- widget-libraries/ant-design/metadata.js
+++ widget-libraries/ant-design/metadata.js
@@ -8,7 +8,13 @@ export default {
   empty_thumb_url: "https://animaapp.s3.amazonaws.com/widgets/ant-design.svg",
   sample_file_figma_url: "",
   styles_import: ["antd/dist/antd.css", "antd/dist/antd.variable.css"],
-  code_template: 'import React from \"react\";\r\nimport ReactDOM from \"react-dom\";\r\nimport * as AntDesignIcons from \"@ant-design\/icons\";\r\nimport * as Antd from \"antd\";\r\n<% if(typeof theme != \"undefined\") { %>\r\nimport { ConfigProvider } from \"antd\";\r\n\r\n\tConfigProvider.config({\r\n\t    theme: {\r\n\t        <% if(typeof theme[\"ant-primary-color\"] != \"undefined\") { %> primaryColor: \"<%-theme[\"ant-primary-color\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-secondary-color\"] != \"undefined\") { %> secondaryColor: \"<%-theme[\"ant-secondary-color\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-success-color\"] != \"undefined\") { %> successColor: \"<%-theme[\"ant-success-color\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-warning-color\"] != \"undefined\") { %> warningColor: \"<%-theme[\"ant-warning-color\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-error-color\"] != \"undefined\") { %> errorColor: \"<%-theme[\"ant-error-color\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-font-size-base\"] != \"undefined\") { %> fontSizeBase: \"<%-theme[\"ant-font-size-base\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-border-radius-base\"] != \"undefined\") { %> borderRadiusBase: \"<%-theme[\"ant-border-radius-base\"]%>\", <% } %>\r\n\t        <% if(typeof theme[\"ant-border-color-base\"] != \"undefined\") { %> borderColorBase: \"<%-theme[\"ant-border-color-base\"]%>\", <% } %>\r\n\t    }\r\n\t});\r\n<% } %>\r\n\r\nconst Main = args => {\r\n    return (\r\n    \t<% if(typeof theme != \"undefined\") { %><ConfigProvider><% } %>\r\n\t\t\t<Example {...args}>\r\n\t\t\t<\/Example>\r\n\t\t<% if(typeof theme != \"undefined\") { %><\/ConfigProvider><% } %>\r\n    );\r\n};\r\n\r\n${storyCode}\r\n\r\n\r\nconst args = <%-params%>;\r\n\r\n\r\nReactDOM.render(<Example {...args} \/>, document.querySelector(\".${nodeClass}\"));',   
+  code_template: 'import React from \"react\";\r\nimport ReactDOM from \"react-dom\";\r\nimport * as AntDesignIcons from \"@ant-design/icons\";\r\nimport * as Antd from \"antd\";\r\n\r\n<%-theme_template%>\r\n\r\nconst Main = args => {\r\n    <%-variableDeclarationCode%>\r\n    return (\r\n    \t<%-theme_prefix%>\r\n\t<%-storyCode%>\r\n\t<%-theme_suffix%>\r\n    );\r\n};\r\n\r\nconst args = <%-params%>;\r\n\r\n\r\nReactDOM.render(<Main {...args} />, document.querySelector(\".${nodeClass}\"));',   
+  code_template_params: {
+    theme_prefix: "<Antd.ConfigProvider>",
+    theme_suffix: "</Antd.ConfigProvider>",
+    dependencies: ["antd"],
+    theme_template: "Antd.ConfigProvider.config({\r\n      theme: {\r\n          primaryColor: \"<%-theme[\"ant-primary-color\"]%>\",\r\n          secondaryColor: \"<%-theme[\"ant-secondary-color\"]%>\",\r\n          successColor: \"<%-theme[\"ant-success-color\"]%>\",\r\n          warningColor: \"<%-theme[\"ant-warning-color\"]%>\",\r\n          errorColor: \"<%-theme[\"ant-error-color\"]%>\",\r\n          fontSizeBase: \"<%-theme[\"ant-font-size-base\"]%>\",\r\n          borderRadiusBase: \"<%-theme[\"ant-border-radius-base\"]%>\",\r\n          borderColorBase: \"<%-theme[\"ant-border-color-base\"]%>\",\r\n      }\r\n  });"
+  },
   theme: {
     categories: [
       {
@@ -43,34 +49,6 @@ export default {
             type: "css-color",
             default: "#f5222d",
             variable: "ant-error-color",
-          },
-        ],
-      },
-      {
-        name: "Typography",
-        params: [
-          {
-            name: "Base font size",
-            type: "css-length",
-            default: 14,
-            variable: "ant-font-size-base",
-          },
-        ],
-      },
-      {
-        name: "Border",
-        params: [
-          {
-            name: "Base radius",
-            type: "css-length",
-            default: 8,
-            variable: "ant-border-radius-base",
-          },
-          {
-            name: "Base color",
-            type: "css-color",
-            default: "#d9d9d9",
-            variable: "ant-border-color-base",
           },
         ],
       },
