2eba9ad19eff82271f694f1a4de59ffae6f404ad
Besart Marku <besart.marku@gmail.com>
2022-09-28 15:46:25 +0200

    feat(mantine): Add ActionIcon

---
+++ widget-libraries/mantine-ui/stories/ActionIcon.stories.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import * as Mantine from "@mantine/core";
+
+export default {
+  title: "Mantine/ActionIcon",
+  component: Mantine.ActionIcon,
+  parameters: {
+    docs: {
+      description: {
+        component: "Action Icon",
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      description: "Variant",
+      control: { type: "select" },
+      options: [
+        "outline",
+        "white",
+        "light",
+        "default",
+        "filled",
+        "subtle",
+        "gradient",
+      ],
+    },
+    //    icon : todo
+    iconSize: { description: "Icon size", type: "number" },
+    size: {
+      description: "Size",
+      control: { type: "select" },
+      options: ["xs", "sm", "md", "lg", "xl"],
+    },
+    loading: { description: "Loading", type: "boolean" },
+    disabled: { description: "Disabled", type: "boolean" },
+  },
+};
+
+const Template = (args) => {
+  return (
+    <Mantine.ActionIcon
+      variant={args.variant}
+      icon={args.icon}
+      iconSize={args.iconSize}
+      size={args.size}
+      loading={args.loading}
+      disabled={args.disabled}
+    />
+  );
+};
+
+export const SimpleActionIcon = Template.bind({});
+
+SimpleActionIcon.args = {
+  variant: "default",
+  //  icon: to do
+  iconSize: 20,
+  size: "md",
+  loading: false,
+  disabled: false,
+};
+
+SimpleActionIcon.storyInfo = {
+  name: "SimpleActionIcon",
+  kind: "Mantine/ActionButton",
+};
