65847d7baf8efb9d7641c2e54c482d518f849b66
Besart Marku <besart.marku@gmail.com>
2022-09-28 15:50:27 +0200

    feat(mantine): add ColorPicker

---
+++ widget-libraries/mantine-ui/stories/ColorPicker.stories.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import * as Mantine from "@mantine/core";
+
+export default {
+  title: "Mantine/ColorPicker",
+  component: Mantine.ColorPicker,
+  parameters: {
+    docs: {
+      description: {
+        component: "ColorPicker",
+      },
+    },
+  },
+  argTypes: {
+    value: { description: "Value", type: "string" },
+    withPicker: { description: "With picker", type: "boolean" },
+    alphaLabel: { description: "Alpha label", type: "string" },
+    defaultValue: { description: "Default value", type: "string" },
+    focusable: { description: "Focusable", type: "boolean" },
+    format: {
+      description: "Format",
+      control: { type: "select" },
+      options: ["hex", "rgba", "rgb", "hsl", "hsla"],
+    },
+    fullWidth: { description: "Full width", type: "boolean" },
+    hueLabel: { description: "Hue label", type: "string" },
+    saturationLabel: { description: "Saturation label", type: "string" },
+    size: {
+      description: "Size",
+      control: { type: "select" },
+      options: ["xs", "sm", "md", "lg", "xl"],
+    },
+    swatches: { description: "Swatches", type: "string" },
+    swatchesPerRow: { description: "Swatches per row", type: "number" },
+  },
+};
+
+const Template = (args) => {
+  return (
+    <Mantine.ColorPicker
+      value={args.value}
+      withPicker={args.withPicker}
+      alphaLabel={args.alphaLabel}
+      defaultValue={args.defaultValue}
+      focusable={args.focusable}
+      format={args.format}
+      fullWidth={args.fullWidth}
+      hueLabel={args.hueLabel}
+      saturationLabel={args.saturationLabel}
+      size={args.size}
+      swatches={args.swatches}
+      swatchesPerRow={args.swatchesPerRow}
+    />
+  );
+};
+
+export const SimpleColorPicker = Template.bind({});
+
+SimpleColorPicker.args = {
+  value: "rgba(47, 119, 150, 0.93)",
+  withPicker: true,
+  alphaLabel: "",
+  defaultValue: "rgba(47, 119, 150, 0.93)",
+  focusable: false,
+  format: "rgba",
+  fullWidth: false,
+  hueLabel: "",
+  saturationLabel: "",
+  size: "md",
+  swatches: "",
+  swatchesPerRow: 10,
+};
+
+SimpleColorPicker.storyInfo = {
+  name: "SimpleColorPicker",
+  kind: "Mantine/ColorPicker",
+};
