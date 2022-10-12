9c2f1f1d7eb5ba57b92ee5442baa24f6cc3944fa
Besart Marku <besart.marku@gmail.com>
2022-10-12 13:39:49 +0200

    feat(mantine): add Accordion and Accordion.Item

---
+++ widget-libraries/mantine-ui/stories/AccordionItem.stories.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import * as MantineCore from "@mantine/core";
+
+export default {
+  title: "Mantine/AccordionItem",
+  component: MantineCore.Accordion.Item,
+  parameters: {
+    docs: {
+      description: {
+        component: "Item",
+      },
+    },
+    container: MantineCore.Accordion,
+  },
+  argTypes: {
+    value: { description: "Value", type: "string" },
+    label: { description: "Label", type: "string" },
+    title: { description: "Title", type: "string" },
+    description: { description: "Description", type: "string" },
+  },
+};
+
+const Template = (args) => {
+  return (
+    <MantineCore.Accordion.Item value={args.value}>
+      <MantineCore.Accordion.Control>
+        {args.title}
+      </MantineCore.Accordion.Control>
+      <MantineCore.Accordion.Panel>
+        {args.description}
+      </MantineCore.Accordion.Panel>
+    </MantineCore.Accordion.Item>
+  );
+};
+
+export const SimpleAccordionItem = Template.bind({});
+
+SimpleAccordionItem.args = {
+  value: "Value",
+  label: "Label",
+  title: "Title",
+  description: "Description",
+};
+
+SimpleAccordionItem.storyInfo = {
+  name: "SimpleAccordionItem",
+  kind: "Mantine/AccordionItem",
+};
