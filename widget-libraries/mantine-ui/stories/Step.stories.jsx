46edde211ed73fac8e583c21c6d10202ea9f2129
Besart Marku <besart.marku@gmail.com>
2022-10-12 13:57:23 +0200

    feat(mantine): add Stepper

---
+++ widget-libraries/mantine-ui/stories/Step.stories.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import * as MantineCore from '@mantine/core';
+
+export default {
+  title: 'Mantine/Step',
+  component: MantineCore.Stepper.Step,
+
+  argTypes: {
+    allowStepClick: { description: 'Allow step click', type: 'boolean' },
+    orientation: {
+      description: 'Orientation',
+      control: { type: 'select' },
+      options: ['horizontal', 'vertical'],
+    },
+    size: {
+      description: 'Size',
+      control: { type: 'select' },
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+    },
+    state: {
+      description: 'State',
+      control: { type: 'select' },
+      options: ['stepInactive', 'stepProgress', 'stepCompleted'],
+    },
+    withIcon: { description: 'With Icon', type: 'number' },
+    label: { description: 'Label', type: 'string' },
+
+  },
+};
+
+
+
+const Template = (args) => {
+  return (
+    <MantineCore.Stepper.Step
+      allowStepClick={args.allowStepClick}
+      orientation={args.orientation}
+      size={args.size}
+      state={args.state}
+      withIcon={args.withIcon}
+      label={args.label}
+      //add description prop
+      >
+        {args.label}
+      </MantineCore.Stepper.Step>
+   
+  );
+};
+
+export const SimpleStep = Template.bind({});
+
+SimpleStep.args = {
+  allowStepClick: false,
+  orientation: 'horizontal',
+  size: 'md',
+  state: 'stepProgress',
+  withIcon: 20,
+  label: 'Label',
+ 
+};
+
+SimpleStep.storyInfo = {
+  name: "SimpleStep",
+  kind: "Mantine/Step",
+};
