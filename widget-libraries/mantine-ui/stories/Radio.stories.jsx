0ec887666300c1158006a99b396a961407767903
Besart Marku <besart.marku@gmail.com>
2022-10-22 22:47:45 +0200

    fix(mantine): remove controlled from radio

--- widget-libraries/mantine-ui/stories/Radio.stories.jsx
+++ widget-libraries/mantine-ui/stories/Radio.stories.jsx
@@ -18,14 +18,18 @@ export default {
 };
 
 const Template = (args) => {
-  const [checked, setChecked] = React.useState(args.value);
-  return <MantineCore.Radio label={args.label} value={args.value} />;
+  return (
+    <MantineCore.Radio
+      label={args.label}
+      value={args.value} />
+  );
 };
 
 export const SimpleRadio = Template.bind({});
 
 SimpleRadio.args = {
   label: "Label",
+  value: "react",
 };
 
 SimpleRadio.storyInfo = {
